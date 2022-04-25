cec3b0b56daeaf1cd31476314ef6fdf4cd90f94a
KristofferSandvang <kristoffersandvang@gmail.com>
2022-04-25 20:08:52 +0200

    preperation for tommorow's work

--- Breakout/Block.cs
+++ Breakout/Block.cs
@@ -1,1 +1,10 @@
+using DIKUArcade.Entities;
+using DIKUArcade.Graphics;
+using System.Collections.Generic;
+using System.IO;
 
+namespace Breakout{
+    public class Block : Entity {
+        public Block(DynamicShape Shape, IBaseImage image) : base(Shape, image) {}
+    }
+}
--- Breakout/Game.cs
+++ Breakout/Game.cs
@@ -1,1 +1,10 @@
+using DIKUArcade;
+using DIKUArcade.GUI;
+namespace Breakout {
+    public class Game : DIKUGame {
+        public Game(WindowArgs windowArgs) : base(windowArgs) {}
 
+        public override void Render() {}
+        public override void Update() {}
+    }
+}
