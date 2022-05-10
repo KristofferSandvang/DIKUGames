b65900b18a7075e9fc99ad0fec20554994d37f2e
LukeBomber <95424128+LukeBomber@users.noreply.github.com>
2022-05-10 17:11:11 +0200

    added switchblock.cs and switchblockReciever.cs classes

---
+++ Breakout/Block/SwitchBlock.cs
@@ -0,0 +1,33 @@
+using DIKUArcade.Entities;
+using DIKUArcade.Graphics;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Breakout.Blocks{
+    public class SwitchBlock : BreakoutBlock {
+        public override int hitPoints {get {return hitPoints;} }
+        public override int value {get {return value;} }
+        private SwitchRecieverBlock[] SwitchRecieverList; 
+        public override void Hit() {
+            hitPoints -= 2;
+            foreach (SwitchRecieverBlock b in SwitchRecieverList) {
+                b.delete;
+                //Skal der tilføjes score når de dør? 
+            }
+        }
+        public override void Dead() {
+            if(hitPoints <= 0) {
+                this.delete; 
+                //Removes from entity container
+                //[Metode der tilføjer score?]
+            }
+        }
+        public void AddRecievers(EntityContainer<SwitchRecieverBlock> e) {
+            foreach (SwitchRecieverBlock b in e) {
+                SwitchRecieverList.Add(b); 
+            }
+        }
+
+       public SwitchBlock(DynamicShape Shape, IBaseImage image) : base(Shape, image) {}
+    } 
+}
---
+++ Breakout/Block/SwitchRecieverBlock.cs
@@ -0,0 +1,25 @@
+using DIKUArcade.Entities;
+using DIKUArcade.Graphics;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Breakout.Blocks{
+    public class SwitchRecieverBlock : BreakoutBlock {
+        public override int hitPoints {get {return hitPoints;} }
+        public override int value {get {return value;} }
+        public override void Hit() {
+        }
+        //public void DeadSwitch() {
+        //hitPoints = 0;
+        //}
+        public override void Dead() {
+            /*if(hitPoints <= 0) {
+                //remove from entity container
+            }*/
+            //Det klarer Switch selv
+        }
+        public SwitchRecieverBlock(DynamicShape Shape, IBaseImage image) : base(Shape, image) {}
+    }
+}
+//Den her class eksisterer så Switch er sikker på at få en liste af blocks der 
+//1. ikke tager skade 2. er dem der skal slettes når Switch bliver ramt
