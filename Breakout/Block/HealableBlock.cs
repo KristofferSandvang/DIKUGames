4db47a97f652f61bff3433146846eb03722b2029
Jimmy Huynh <JHU@cabinplant.com>
2022-05-11 19:40:22 +0200

    HealableBlock have been worked on further

--- Breakout/Block/HealableBlock.cs
+++ Breakout/Block/HealableBlock.cs
@@ -1,29 +1,32 @@
-/*using DIKUArcade.Entities;
+using DIKUArcade.Entities;
 using DIKUArcade.Graphics;
 using System.Collections.Generic;
 using System.IO;
 
 namespace Breakout.Blocks{
     public class HealableBlock : BreakoutBlock {
-        public override int hitPoints {get {return hitPoints;} }
-        public override int value {get {return value;} }
-
+        public DynamicShape shape;
+        public HealableBlock(DynamicShape Shape, IBaseImage image) : base(Shape, image) {
+            hitPoints = 10;
+            value = 200;
+            shape = Shape;
+        }
+        public int GetHP(){
+            return hitPoints;
+        }
         public override void Hit() {
-            hitPoints -= 5;
-            //If can be broken once every other block gone, implement if statement
+           hitPoints -= 5;
         }
-        public override void Dead() {
+        public override bool Dead() {
             if (hitPoints <= 0) {
+              //  score = score + value;
                 return true;
-            }
-            else return false;
-            //add score    
-            //remove from entity container
+            } else {return false;}
         }
-
-        public iterateHeal(){
-            while 
+        public void iterateHeal() {
+            //healEvent = new TimedGameEvent.HasExpired(); //Figure it out later
+            //if (healEvent == true){hitPoints += 2;}
+            //if TimeGameEvent has expired then increment hp
         }
-        public HealableBlock(DynamicShape Shape, IBaseImage image) : base(Shape, image) {}
     }
-}*/
+}
