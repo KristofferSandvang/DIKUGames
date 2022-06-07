9306f8b83db014e81dffe6deca5412d9f6f71c2b
KristofferSandvang <kristoffersandvang@gmail.com>
2022-06-07 02:13:23 +0200

    Added documentation and fixed lesser errors

--- Breakout/PowerUp/PowerUpFactories/ExtraWidthFactory.cs
+++ Breakout/PowerUp/PowerUpFactories/ExtraWidthFactory.cs
@@ -5,6 +5,12 @@ using DIKUArcade.Entities;
 
 namespace Breakout.PowerUps.PowerUpFactories {
     public class ExtraWidthFactory : PowerUpFactory {
+        /// <summary>
+        /// Creates a PowerUP
+        /// </summary>
+        /// <returns>
+        /// A PowerUP
+        /// </returns>
         public override PowerUp CreatePowerUp(Vec2F pos) {
             return new ExtraWidth(new DynamicShape(pos, new Vec2F(0.05f, 0.05f)), extraWidthImg);
         }
